8e4a23b73a3c00791c3d0e7c9b54176d086253ef
Игорь <igorgrebenyuk2008@gmail.com>
2026-07-19 20:26:46 +0300

    Создал пару сущностей и почти доделал слой ДАЛ и контекста

---
+++ CarDeliveryAndAcceptance.Entities/Car.cs
@@ -0,0 +1,54 @@
+﻿using CarDeliveryAndAcceptance.Dal.Contracts;
+
+namespace CarDeliveryAndAcceptance.Entities
+{
+    public class Car : BaseAuditEntity
+    {
+        /// <summary>
+        /// Уникальный идентификатор автомобиля
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Марка, модель
+        /// </summary>
+        public string MakeAndModel { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Регистрационный знак 
+        /// </summary>
+        public string LicensePlate { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Идентификационный номер
+        /// </summary>
+        public string VinCode { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Год выпуска
+        /// </summary>
+        public int ManufactureYear { get; set; }
+
+        /// <summary>
+        /// Двигатель №
+        /// </summary>
+        public string EngineNumber { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Шасси (рама)
+        /// </summary>
+        public string ChassisNumber { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Кузов (коляска)
+        /// </summary>
+        public string BodyNumber { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Цвет
+        /// </summary>
+        public string Color { get; set; } = string.Empty;
+
+        
+    }
+}
---
+++ CarDeliveryAndAcceptance.Entities/City.cs
@@ -0,0 +1,16 @@
+﻿using CarDeliveryAndAcceptance.Dal.Contracts;
+
+namespace CarDeliveryAndAcceptance.Entities;
+
+public class City : BaseAuditEntity
+{
+    /// <summary>
+    /// Уникальный идентификатор города
+    /// </summary>
+    public Guid Id { get; set; }
+    
+    /// <summary>
+    /// Название города
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+}
